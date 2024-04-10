17ff989ddac215370cce729b4a6dccdd12bbe450
Vihaan Shah <65532341+Vihaanshah29@users.noreply.github.com>
2024-04-10 13:19:40 -0400

    first commit

---
+++ src/main/java/edu/iu/prebello/historyservice/controllers/PrimesHistoryController.java
@@ -0,0 +1,24 @@
+package edu.iu.prebello.historyservice.controllers;
+
+import edu.iu.prebello.historyservice.model.PrimesRecord;
+import edu.iu.prebello.historyservice.repository.PrimesHistoryRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/primes")
+public class PrimesHistoryController {
+    private final PrimesHistoryRepository primesHistoryRepository;
+
+    public PrimesHistoryController(PrimesHistoryRepository primesHistoryRepository) {
+        this.primesHistoryRepository = primesHistoryRepository;
+    }
+
+    @GetMapping("/{customer}")
+    public List<PrimesRecord> findAll(@PathVariable String customer) {
+        return primesHistoryRepository.findAllByCustomer(customer);
+    }
+}
+
